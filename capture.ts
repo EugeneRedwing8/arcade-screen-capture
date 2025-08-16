cca74ffd3de8b810344bb2e9d7f57cb215c9dbd8
EugeneRedwing8 <emersonejohnson@gmail.com>
2025-08-15 19:51:04 -0700

    Initial files for MakeCode project

---
+++ capture.ts
@@ -0,0 +1,9 @@
+//%color="#000090"
+//%icon="\uf03d"
+namespace capture{
+    //%group="image"
+    //%block="capture screen image"
+    export function capture_screen_image() {
+        
+    }
+}
---
+++ main.ts
@@ -0,0 +1,1 @@
+capture.capture_screen_image()
